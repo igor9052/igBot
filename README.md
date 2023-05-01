611258c12924a22beae8f7d7ff3d635e5f7c12ef
Igor Kuzmenko <igor905.2@gmail.com>
2023-05-01 18:49:50 -0400

    Add initial iBot code. Add basic info to README. Add kbot.Handle(telebot.OnText, func(m telebot.Context) function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # igBot
-PrometheusOrgUA 
+This is an implementation of the practice task within the Prometheus (https://prometheus.org.ua) "DevOps basics" course.
+
+Bot: http://t.me/igor9052_bot
+
+Available commands:
+- "hello"
+- "Слава Україні!"
--- cmd/ibot.go
+++ cmd/ibot.go
@@ -6,14 +6,25 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+const envToken = "TELE_TOKEN"
+
+var (
+	botToken string = os.Getenv(envToken)
 )
 
 // ibotCmd represents the ibot command
 var ibotCmd = &cobra.Command{
-	Use:   "ibot",
-	Short: "A brief description of your command",
+	Use:     "ibot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +32,36 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ibot called")
+		fmt.Printf("ibot %s started", appVersion)
+		ibot, err := telebot.NewBot(telebot.Settings{
+			URL:   "",
+			Token: botToken,
+			Poller: &telebot.LongPoller{
+				Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check your telegram token (%s): %v", envToken, err)
+		}
+
+		ibot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm iBot %s!", appVersion))
+
+			case "Слава Україні!":
+				err = m.Send(fmt.Sprintf("Герям слава!"))
+			}
+
+			return err
+		})
+
+		ibot.Start()
+
 	},
 }
 
